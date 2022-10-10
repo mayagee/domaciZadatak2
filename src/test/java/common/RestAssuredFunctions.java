5737ea9cd70b195cf7414ea08047a836c4da030d
Maja Georgijevski <maya.ge@live.com>
2022-10-10 11:03:58 +0200

    commit domaci 02

--- src/test/java/common/RestAssuredFunctions.java
+++ src/test/java/common/RestAssuredFunctions.java
@@ -2,9 +2,7 @@ package common;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-
 import static io.restassured.RestAssured.given;
-
 public class RestAssuredFunctions {
 
     public static Response get(String url) {
